5c74d3389764169bdca9c33f23db570296ff678a
iijima <h.iijima0202@gmail.com>
2017-06-22 00:33:46 +0900

    update

--- src/compile_all.py
+++ src/compile_all.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from jinja2 import Template, Environment, FileSystemLoader
 
+fname_main = 'index'
+
 env = Environment(loader=FileSystemLoader('.'), trim_blocks=False)
-template = env.get_template('index_tpl.html')
+template = env.get_template(fname_main+'_tpl.html')
 disp_text = template.render()
-print(disp_text)
+
+# # stdout
+# print(disp_text)
+
+# output file
+f = open('../'+fname_main+'.html', 'w')
+f.write(disp_text)
+f.close()
