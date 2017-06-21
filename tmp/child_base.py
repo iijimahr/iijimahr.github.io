f5f7fb056a4080cb5991090a53f6314e76084724
iijima <h.iijima0202@gmail.com>
2017-06-22 01:01:30 +0900

    update

--- tmp/child_base.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from jinja2 import Template, Environment, FileSystemLoader
-
-env = Environment(loader=FileSystemLoader('.'), trim_blocks=False)
-template = env.get_template('child.tpl')
-disp_text = template.render()
-print(disp_text)
