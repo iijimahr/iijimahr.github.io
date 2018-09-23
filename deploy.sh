76d54dd6a61fd9ac08b82028360fbd3d7d1b36bf
iijima <h.iijima0202@gmail.com>
2018-09-23 15:59:24 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo ./compile_all.py
-cd src; ./compile_all.py; cd -
+cd src
+./compile_all.py
+cd -
+echo ./compress_html.sh
 ./compress_html.sh
+echo ./update.sh
 ./update.sh
